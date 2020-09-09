eaff6a6f3f8d5136ccd916ab5cb8caae18d1940b
ManliestBen <benjamintmanley@gmail.com>
2020-09-09 14:22:49 -0500

    display class details

--- src/pages/ClassDetails/ClassDetails.jsx
+++ src/pages/ClassDetails/ClassDetails.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import * as APIMethods from '../../services/api-calls'
+
+// Or you can write it this way!:
+// import { getDetails } from "../../services/api-calls";
 
 class ClassDetails extends Component {
   state = { 
@@ -8,15 +12,39 @@ class ClassDetails extends Component {
 
    async componentDidMount() {
      // This is where we call the API
-     // Then set state
-   }
+     const classDetails = await APIMethods.getDetails(this.state.url)
 
-  render() { 
+    // The other way:
+    // const classDetails = await getDetails(this.state.url)
+
+     // Then set state
+    this.setState({ classDetails })
+    }
+
+  render() {
+    const { classDetails } = this.state; 
     return ( 
       <div>
-        <h3>Class Details</h3>
+        {/* Look at this sweet ternary! */}
+        {/* This is a great way to render a 'loading' page! */}
+        {classDetails.name ?
+        <>
+          {/* Gotta have the random cat pics... */}
+          <img src="http://theoldreader.com/kittens/320/240/" alt=""/>
+          <h2>{classDetails.name}</h2>
+          <div>Hit die: d{classDetails.hit_die}</div>
+          <h3>Proficiencies:</h3>
+          {classDetails.proficiencies.map((proficiency) => (
+            <div key={proficiency.index}>{proficiency.name}</div>
+          ))}
+        </>
+        :
+        <>
+          <p>Loading class details...</p>
+        </>
+        }
       </div>
-     );
+    );
   }
 }
  
