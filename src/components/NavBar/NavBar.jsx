365964d33cbfc8df87f6aaca8d7e6377cc26df31
ManliestBen <benjamintmanley@gmail.com>
2020-09-09 10:14:29 -0500

    add navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = (props) => {
+  return (
+    <header className="App-header">
+      <img 
+        src="/images/logo.svg" 
+        alt="logo"
+        className="App-logo"
+        style={{ width: "100px", height: "100px"}}
+      />
+
+    </header>
+  )
+}
+
+export default NavBar
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import NavBar from '../../components/NavBar/NavBar'
 
-function App() {
-  return (
-    <>
-      <div>
-        App Component
-      </div>
-    </>
-  );
+class App extends Component {
+  state = {
+    navItems: [
+      {url: "/classlist", name: "Class List"}, 
+      {url: "/monsterlist", name: "Scary Monsters"}, 
+      {url: "/spellSearch", name: "Search for Spells"}
+    ]
+  }
+
+  render() {
+    return (
+      <>
+        <NavBar 
+          navItems={this.state.navItems}
+        />
+      </>
+    )
+  }
 }
 
 export default App;
