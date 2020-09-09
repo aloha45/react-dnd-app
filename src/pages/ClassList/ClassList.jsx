38ad80fd01d0bf69928c5d9fc519b0a7232b2171
ManliestBen <benjamintmanley@gmail.com>
2020-09-09 12:55:53 -0500

    add class info

--- src/pages/ClassList/ClassList.jsx
+++ src/pages/ClassList/ClassList.jsx
@@ -11,9 +11,18 @@ class ClassList extends Component {
   render() {
     return (
       <div>
-        <h3>I'm the Class List!</h3>
+        <h3>Class List</h3>
+        <div className="icon-container">
+          {/* This is where we map over our results, representing each with a div containing an image and a name */}
+          {this.state.results.map((classTitle) => (
+            <div id="classDiv" key={classTitle}>
+              <img style={{ width: "100px", height: "100px" }} src={`/images/${classTitle}.svg`} alt="class-logo"/>
+              {classTitle}
+            </div>
+          ))}
+        </div>
       </div>
-    )
+    );
   }
 }
 
